3c6527d8b43840142c9f6f7272a91ffa9191e304
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-21 01:22:23 +0530

    Use environment-based API URL

--- src/app/features/report/services/report.service.ts
+++ src/app/features/report/services/report.service.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -13,7 +14,7 @@ export interface ReportFilters {
 })
 export class ReportService {
 
-  private readonly apiUrl = 'http://localhost:8080/api/report';
+  private readonly apiUrl = environment.apiUrl;
   private readonly storageKey = 'inspectionReport';
 
   constructor(private http: HttpClient) {}
