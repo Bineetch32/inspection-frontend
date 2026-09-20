d0d1e5467a24485bf7b632831e8ae6b285eb5a31
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-20 23:57:35 +0530

    Build inspection validation result page

--- src/app/features/inspection/components/validation-result/validation-result.component.ts
+++ src/app/features/inspection/components/validation-result/validation-result.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ReportService } from '../../../report/services/report.service';
 
 @Component({
   selector: 'app-validation-result',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './validation-result.component.html',
   styleUrl: './validation-result.component.css'
 })
-export class ValidationResultComponent {
+export class ValidationResultComponent implements OnInit {
 
+  report: any = null;
+
+  constructor(
+    private reportService: ReportService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.report = this.reportService.getReport();
+
+    if (!this.report) {
+      this.router.navigate(['/inspection/upload']);
+    }
+  }
+
+  openReport(): void {
+    if (this.report?.reportGenerationAllowed) {
+      this.router.navigate(['/report']);
+    }
+  }
 }
