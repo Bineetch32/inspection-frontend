e8b28a78e8805088fcce65f046edb314b97c90cf
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-20 22:28:02 +0530

    Connect Part Master frontend to backend API

--- src/app/features/part-master/services/part-master.service.ts
+++ src/app/features/part-master/services/part-master.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface PartMaster {
+  id?: number;
+  partNo: string;
+  partName: string;
+  vendorCode: string;
+  vendorName: string;
+  model: string;
+  packaging: string;
+  active: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartMasterService {
 
-  constructor() { }
+  private readonly apiUrl = 'http://localhost:8080/api/parts';
+
+  constructor(private http: HttpClient) {}
+
+  getAllParts(): Observable<PartMaster[]> {
+    return this.http.get<PartMaster[]>(this.apiUrl);
+  }
+
+  getPart(id: number): Observable<PartMaster> {
+    return this.http.get<PartMaster>(`${this.apiUrl}/${id}`);
+  }
+
+  addPart(part: PartMaster): Observable<PartMaster> {
+    return this.http.post<PartMaster>(this.apiUrl, part);
+  }
+
+  updatePart(id: number, part: PartMaster): Observable<PartMaster> {
+    return this.http.put<PartMaster>(`${this.apiUrl}/${id}`, part);
+  }
+
+  deletePart(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
