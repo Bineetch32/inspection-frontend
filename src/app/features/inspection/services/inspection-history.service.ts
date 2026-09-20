259950601de919ca0ab5c0d9351588cd919d9089
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-21 01:22:31 +0530

    Use environment-based API URL

--- src/app/features/inspection/services/inspection-history.service.ts
+++ src/app/features/inspection/services/inspection-history.service.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -26,7 +27,7 @@ export interface InspectionHistoryRecord {
 })
 export class InspectionHistoryService {
 
-  private readonly apiUrl = 'http://localhost:8080/api/inspections';
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
